16c5785f7e7e7da40ef2e3c2b4209920f29f3069
Bimlendra <bimlendrayadav2075@gmail.com>
2023-05-20 19:43:12 +0545

    forget password reset link

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,100 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home Sewa</title>
+    <meta name="description" content="Home Sewa">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/cs-skin-elastic.css') }}">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/scss/style.css') }}">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+
+</head>
+<body class="bg-dark">
+
+
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="index.html">
+                        <img class="align-content" src="{{asset('images/homesewalogo.png')}}" alt="" style="width:70px; height:80px;">
+                    </a>
+                </div>
+                <div class="login-form">
+                <div style="display:flex; flex-direction:column;  margin:auto; width:85%;">
+                    <h5><strong>Reset Your New Password</strong></h5>
+                </div>
+                <form method="POST" action="{{ route('password.store') }}">
+                    @csrf
+
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                    <!-- Email-->
+                    <div class="" style="padding:5px; 30px; 20px; display:flex; flex-direction:column; margin:auto; width:89%;">
+                        <label>Email address</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email', $request->email) }}" required autofocus autocomplete="username">
+                        @if ($errors->has('email'))
+                            <span class="help-block" style="color:red">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <!-- Password -->
+                    <div class="" style="padding:5px; 30px; 20px; display:flex; flex-direction:column; margin:auto; width:89%;">
+                        <label>Password</label>
+                        <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                        @if ($errors->has('email'))
+                            <span class="help-block" style="color:red">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="" style="padding:5px; 30px; 20px; display:flex; flex-direction:column; margin:auto; width:89%;">
+                        <label>Confirm Password</label>
+                        <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        @if ($errors->has('email'))
+                            <span class="help-block" style="color:red">
+                                <strong>{{ $errors->first('password_confirmation') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30" style="margin-left: 6%; width: 87%;">Reset Password</button>
+                </form>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+    <script src="{{ asset('admin/assets/js/vendor/jquery-2.1.4.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/plugins.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/main.js') }}"></script>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+</body>
+</html>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
